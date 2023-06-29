26542a41df8730fbb5ffcba44adfac212886a837
muhammad riduwan <muhammadriduwan4747@gmail.com>
2023-06-29 11:14:05 +0700

    add spalsh screen

---
+++ app/src/main/java/app/aplikasiuts/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package app.aplikasiuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MenuUtamaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+
+    }
+}
